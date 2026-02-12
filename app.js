b219aab53a7ebb2724fc1cffbf2fab06fff932e0
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2026-02-12 23:43:08 +0530

    frontend url added

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://reactalk.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   }),
